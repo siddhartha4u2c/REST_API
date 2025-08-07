cb5ee59f7cd2773c658914a4c4c48a7dffada5a1
siddhartha_borpuzari <siddhartha_borpuzari@rediffmail.com>
2025-08-07 23:44:32 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return '🎉 Hello from Flask on Heroku!'
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Welcome to the Flask API!'})
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({'message': 'Hello from Flask API!'})
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    result = data['a'] + data['b']
+    return jsonify({'result': result})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
